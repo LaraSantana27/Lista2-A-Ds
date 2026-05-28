6686a51df14f92603072a358157685a314f2cedc
LaraSantana27 <llarasantana27@gmail.com>
2026-05-28 15:46:29 -0300

    POO
    hoje fizemos um exercício com exemplo de POO

---
+++ src/POO/ExermploPOO.ts
@@ -0,0 +1,30 @@
+//Atributos
+class Funcionario{
+    nome: string;
+    cargo: string;
+    salario: number;
+
+//Constructor
+    constructor(n: string, c: string, s: number){
+        this.nome = n;
+        this.cargo = c;
+        this.salario = s;
+    }
+
+//Método
+    exibirResumo(): void {
+        console.log(`Funcionário: ${this.nome} | Cargo: ${this.cargo} | Salário: ${this.salario}`)
+    }
+}
+let continuar: string = ""
+
+while(continuar != "N"){
+    let n:string, c:string, s:number
+    n = String(prompt("Informe o nome do Funcionário: "))
+    c = String(prompt("Informe o seu Cargo: "))
+    s = Number(prompt("Informe o seu Salário: "))
+
+    let novoFunc = new Funcionario(n,c,s)
+    novoFunc.exibirResumo()
+    continuar = String(prompt("deseja continuar? S-Sim, N-Não")).toUpperCase()
+}
