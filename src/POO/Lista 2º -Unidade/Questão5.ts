3feb89ec4e832c6f4ca2d84dc9d8c9c16e2d1518
LaraSantana27 <llarasantana27@gmail.com>
2026-06-19 15:25:47 -0300

    Inicio da questão 5

---
+++ src/POO/Lista 2º -Unidade/Questão5.ts
@@ -0,0 +1,20 @@
+// 5. Classe Pessoa: Crie uma classe que modele uma pessoa:
+// 1. Atributos: nome, idade, peso e altura
+// 2. Métodos: Envelhecer, engordar, emagrecer, crescer.
+// Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos,
+// ela deve crescer 0,5 cm.
+
+class Pessoa{
+    nome:string
+    idade:number
+    peso:number
+    altura:number
+
+    constructor(n:string, i:number, p:number, a:number){
+        this.nome=n
+        this.idade=i
+        this.peso=p
+        this.altura=a
+    }
+    
+}
