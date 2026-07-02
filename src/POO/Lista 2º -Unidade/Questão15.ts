1aa84bbb747bc609cf7082c7df77e6f4a1472ab2
LaraSantana27 <llarasantana27@gmail.com>
2026-07-02 15:55:23 -0300

    Resolução da questão 15

---
+++ src/POO/Lista 2º -Unidade/Questão15.ts
@@ -0,0 +1,54 @@
+// 15. Herança Encapsulamento
+// Uma empresa possui dois tipos de funcionários: horistas (pagos por hora trabalhada) e assalariados
+// (salário fixo mensal). Crie uma hierarquia de classes com Funcionario como superclasse e
+// FuncionarioHorista e FuncionarioAssalariado como subclasses. O programa deve solicitar os dados via
+// teclado e calcular o salário de cada um.
+
+abstract class Funcionario{
+    public nome:string
+
+    constructor(nome:string){
+        this.nome=nome
+    }
+    abstract calcularSalario():number
+    
+    exibirSalario(): void{
+        console.log(`Nome: ${this.nome} o seu salário é: ${this.calcularSalario()}`)
+    }
+
+}
+
+class FuncionarioAssalariado extends Funcionario{
+    salarioFixo:number
+
+    constructor(nome:string, salarioFixo:number){
+        super(nome)
+        this.salarioFixo=salarioFixo
+        
+    }
+    calcularSalario(): number{
+        return this.salarioFixo
+    }
+}
+
+class FuncionarioHorista extends Funcionario{
+    valorHoras: number
+    quantHoras: number
+
+    constructor(nome:string,valorHoras:number, quantHoras: number){
+        super(nome)
+        this.valorHoras=valorHoras
+        this.quantHoras=quantHoras
+    }
+    calcularSalario(): number{
+        let salarioFinal = this.quantHoras * this.valorHoras
+        return salarioFinal
+    }
+
+}
+
+let funcionarioASS: FuncionarioAssalariado = new FuncionarioAssalariado("Lara",1900)
+funcionarioASS.exibirSalario()
+
+let funcionarioH: FuncionarioHorista = new FuncionarioHorista("Luiza", 80, 16)
+funcionarioH.exibirSalario()
