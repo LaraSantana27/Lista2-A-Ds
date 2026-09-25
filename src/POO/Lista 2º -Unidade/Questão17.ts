7da4e631cce62dbe8fc0ce78b23f4470f63a2c97
Santos-Dev-iniciante <llarasantana27@gmail.com>
2026-09-25 14:47:17 -0300

    Update Questão17.ts

--- src/POO/Lista 2º -Unidade/Questão17.ts
+++ src/POO/Lista 2º -Unidade/Questão17.ts
@@ -10,122 +10,154 @@
 
 export function questãoPOO17():void{
 
-abstract class Usuario{
-    private _id: number
-    private _nome: string
+    abstract class Usuario{
+        private _id: number
+        private _nome: string
 
-    constructor(id:number, nome:string){
-        this._id=id
-        this._nome=nome
-    }
-    public get id(): number {
-        return this._id
-    }
-    public set id(value: number) {
-        this._id = value
-    }
-    public get nome(): string {
-        return this._nome
-    }
-    public set nome(value: string) {
-        this._nome = value
-    }
+        constructor(id:number, nome:string){
+            this._id=id
+            this._nome=nome
+        }
 
-    abstract identificar(): string
-}
+        public get id(): number {
+            return this._id
+        }
 
-class Aluno extends Usuario{
-    private _curso: string
-    constructor(id:number, nome:string, curso:string){
-        super(id, nome)
-        this._curso=curso
-    }
-    public get curso(): string {
-        return this._curso
-    }
-    public set curso(value: string) {
-        this._curso = value
-    }
-    identificar(): string {
-        return `Aluno ${this.nome} do curso de ${this.curso} almoçou hoje!`
-    }
-}
+        public set id(value: number) {
+            this._id = value
+        }
 
-class Servidor extends Usuario{
-    private _departamento: string
-    constructor(id:number, nome:string, departamento:string){
-        super(id, nome)
-        this._departamento=departamento
-    }
-    public get departamento(): string {
-        return this._departamento
-    }
-    public set departamento(value: string) {
-        this._departamento = value
-    }
-    identificar(): string {
-        return `Servidor ${this.nome} do departamento de ${this.departamento} almoçou hoje!`
-    }
-}
+        public get nome(): string {
+            return this._nome
+        }
 
-let usuarios: Usuario[] = []
-let historico: Usuario[] = []
-let comando = ""
+        public set nome(value: string) {
+            this._nome = value
+        }
 
-while (comando.toLowerCase() !== "encerrar") {
-    comando = String(prompt("Digite 'cadastrar' para novo usuário, 'passar' para registrar acesso ou 'encerrar' para finalizar:"))
+        abstract identificar(): string
 
-    if (comando.toLowerCase() === "cadastrar") {
-        let tipo = String(prompt("Tipo de usuário: 1-Aluno | 2-Servidor")).trim()
-        let id = Number(prompt("Informe o identificador do usuário:"))
-        let nome = String(prompt("Informe o nome completo:"))
-
-        if (tipo === "1") {
-            let curso = String(prompt("Informe o curso do aluno:"))
-            usuarios.push(new Aluno(id, nome, curso))
-        } else if (tipo === "2") {
-            let departamento = String(prompt("Informe o departamento do servidor:"))
-            usuarios.push(new Servidor(id, nome, departamento))
-        } else {
-            alert("Tipo inválido!")
+        exibirResumo(): string {
+            return `ID: ${this.id}
+                Nome: ${this.nome}
+                ${this.identificar()}`
         }
     }
-    else if (comando.toLowerCase() === "passar") {
-        let idBusca = Number(prompt("Informe o identificador do usuário que passou na catraca:"))
-        let usuario: Usuario | undefined = undefined
 
-        for (let i = 0; i < usuarios.length; i++) {
-            if (usuarios[i].id === idBusca) {
-                usuario = usuarios[i]
+    class Aluno extends Usuario{
+        private _curso: string
+
+        constructor(id:number, nome:string, curso:string){
+            super(id, nome)
+            this._curso=curso
+        }
+
+        public get curso(): string {
+            return this._curso
+        }
+
+        public set curso(value: string) {
+            this._curso = value
+        }
+
+        identificar(): string {
+            return `Aluno ${this.nome} do curso de ${this.curso} almoçou hoje!`
+        }
+    }
+
+    class Servidor extends Usuario{
+        private _departamento: string
+
+        constructor(id:number, nome:string, departamento:string){
+            super(id, nome)
+            this._departamento=departamento
+        }
+
+        public get departamento(): string {
+            return this._departamento
+        }
+
+        public set departamento(value: string) {
+            this._departamento = value
+        }
+
+        identificar(): string {
+            return `Servidor ${this.nome} do departamento de ${this.departamento} almoçou hoje!`
+        }
+    }
+
+    let usuarios: Usuario[] = []
+    let historico: Usuario[] = []
+
+    let comando = Number(prompt("Digite 1- para cadastrar novo usuário, 2- para registrar acesso ou 3- para finalizar:"))
+
+    while (comando != 3) {
+
+        if (comando == 1) {
+
+            let tipo = String(prompt("Tipo de usuário: 1-Aluno | 2-Servidor"))
+            let id = Number(prompt("Informe o identificador do usuário:"))
+            let nome = String(prompt("Informe o nome completo:"))
+
+            if (tipo === "1") {
+
+                let curso = String(prompt("Informe o curso do aluno:"))
+
+                usuarios.push(new Aluno(id, nome, curso))
+
+            } else if (tipo === "2") {
+
+                let departamento = String(prompt("Informe o departamento do servidor:"))
+
+                usuarios.push(new Servidor(id, nome, departamento))
+
+            } else {
+                alert("Tipo inválido!")
+            }
+
+        } else if (comando == 2) {
+
+            let idBusca = Number(prompt("Informe o identificador do usuário que passou na catraca:"))
+
+            let usuario: Usuario | undefined = undefined
+
+            for (let i = 0; i < usuarios.length; i++) {
+
+                if (usuarios[i].id === idBusca) {
+                    usuario = usuarios[i]
+                }
+
+            }
+
+            if (usuario) {
+                historico.push(usuario)
+                alert("Acesso registrado!")
+            } else {
+                alert("Usuário não encontrado!")
             }
         }
 
-        if (usuario) {
-            historico.push(usuario)
-            alert("Acesso registrado!")
+        comando = Number(prompt("Digite 1- para cadastrar novo usuário, 2- para registrar acesso ou 3- para finalizar:"))
+    }
+
+    let mensagens = "=== ALMOÇARAM HOJE ===\n"
+
+    let totalAlunos = 0
+    let totalServidores = 0
+
+    for (let i = 0; i < historico.length; i++) {
+
+        mensagens += historico[i].exibirResumo() + "\n"
+
+        if (historico[i] instanceof Aluno) {
+            totalAlunos++
         } else {
-            alert("Usuário não encontrado!")
+            totalServidores++
         }
     }
-}
 
-let mensagens = "=== ALMOÇARAM HOJE ===\n"
-let totalAlunos = 0
-let totalServidores = 0
-
-for (let i = 0; i < historico.length; i++) {
-    mensagens += historico[i].identificar() + "\n"
-
-    if (historico[i] instanceof Aluno) {
-        totalAlunos++
-    } else if (historico[i] instanceof Servidor) {
-        totalServidores++
-    }
-}
-
-alert(`${mensagens}
+    alert(`${mensagens}
 
 Total de acessos de alunos: ${totalAlunos}
 Total de acessos de servidores: ${totalServidores}`)
-
 }
